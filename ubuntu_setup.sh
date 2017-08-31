9bf7c6526715477890429d6eb44ed6fe16482b7a
William Barshop <wbarshop@ucla.edu>
2017-08-31 04:25:58 -0700

    Changes to get sed ready to go...

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,7 +1,8 @@
-echo "You must run this script as root (or otherwise elevated priviliges [eg sudo])"
-echo "ExecStart=/usr/bin/docker daemon -H fd:// -D -H tcp://127.0.0.1:2375" >> /lib/systemd/system/docker.service
+echo "Please make sure to run this script as root (or otherwise elevated priviliges [eg sudo])!\nIf you already are- great. That'll help."
+sed -i 's#^ExecStart=.*$#ExecStart=/usr/bin/dockerd -H fd:// -D -H tcp://127.0.0.1:2375#g' /lib/systemd/system/docker.service
 ufw allow 81
 systemctl daemon-reload
 service docker restart
 sleep 15
 docker pull wbarshop/milkyway_shiny
+echo "We're all done setting up docker to work with ShinyProxy... Be careful not to open external access to port 2375!"
